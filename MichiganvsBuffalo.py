88564e8ac4ff4581cc10fecf9537dc91033014be
jv11699 <janvolta@buffalo.edu>
2018-11-14 01:45:13 -0500

    UPDATED VERSION outputs all the score

--- MichiganvsBuffalo.py
+++ MichiganvsBuffalo.py
@@ -3,11 +3,15 @@ import urllib.request
 import io
 
 # from Tools.scripts.parse_html5_entities import fname
-source = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
+source = urllib.request.urlopen('http://ubbulls.com/sports/wvball/2017-18/files/ubvb28.htm').read()
 x = 12
-soup = BeautifulSoup(source, 'html.parser')
+soup = BeautifulSoup(source, 'lxml')
+ele = soup('font', face="verdana")
+#DRAFT STARTS AT 59
+for i in range(59,len(soup.find_all('tr'))):
+    print(soup.find_all('tr')[i].get_text())
 
-print(soup.find_all('p'))
+
 """
 print(soup.title.text)
 print (soup.p)                THIS IS TESTING TO SEE WHETER BEAUTIFUL SOUP WORKS 
