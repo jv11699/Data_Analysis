8b6212e4f8c63ea9d96c318f63b6d17d032424a9
jv11699 <janvolta@buffalo.edu>
2019-01-05 20:21:37 -0500

    Update fixed some bugs

--- DataUBVB.py
+++ DataUBVB.py
@@ -24,18 +24,18 @@ class Data():
             raise Exception("The years are only from 2017 and 2018")
         self.year = year
         if year == 2017:
-            self.__url = 'http://ubbulls.com/sports/wvball/2017-18/files/teamstat.htm'
+            url = 'http://ubbulls.com/sports/wvball/2017-18/files/teamstat.htm'
         else:
-            self.__url = 'http://ubbulls.com/sports/wvball/2018-19/files/teamstat.htm'
-        url_source = urllib.request.urlopen(self.__url).read() #the url request that gets all the url
+            url = 'http://ubbulls.com/sports/wvball/2018-19/files/teamstat.htm'
+        url_source = urllib.request.urlopen(url).read() #the url request that gets all the url
         self.soup = BeautifulSoup(url_source, 'lxml')  # Conversion to a beautiful soup object
         links = [a for a in (td.find('a', href=True) for td in self.soup.findAll('td')) if a]  # Grabs all the a tags which contains the links
         self.stringLink = []  # Should contain of all the links for the link!
         for link in links:
             if self.year == 2017:
-                self.stringLink.append('http://ubbulls.com/sports/wvball/2017-18/files/' + link['href'])  # Each link is appended inside the StringLink array
+                self.stringLink.append('http://ubbulls.com/sports/wvball/2017-18/files/' + link['href'])  # Each link in the 2017 website is appended inside the StringLink array
             else:
-                self.stringLink.append('http://ubbulls.com/sports/wvball/2018-19/files/' + link['href'])  # Each link is appended inside the StringLink array
+                self.stringLink.append('http://ubbulls.com/sports/wvball/2018-19/files/' + link['href'])  # Each link in the 2018 website is appended inside the StringLink array
 
     def grabScore(self,array):
         '''
@@ -45,7 +45,7 @@ class Data():
         :return: returns an array of the scores if the mode is set on score, and an array of
         '''
 
-        scores = re.findall(r'\d{1,2}-\d{1,2}', self.convBStoString(array))  # using regular expressions
+        scores = re.findall(r'\d{1,2}-\d{1,2}', self.convBStoString(array))  # using regular expressions-it finds pattern of [digits - digits]
         score_arr = []
         for score in scores:  # since regular expression returns a list of all found patterns
             score_arr.append(score)
@@ -59,8 +59,9 @@ class Data():
         :return: data base off the mode, score: will return scores, name: will return the title of that match
         '''
         if match_number > len(self.stringLink):
+            #if user input is irrational for the parameter: match_number
             raise Exception("The match number excedes the number of matches")
-        counter = 0
+        counter = 0 #the counter used to counter the number of titles that will be used in the 'name' mode
         source_scores = urllib.request.urlopen(self.stringLink[match_number]).read()  # the url request that is determined through the match number
         soup_match = BeautifulSoup(source_scores, 'lxml')  # Conversion to a beautiful soup object
         scores = [b for b in (td.find('b') for td in soup_match.findAll('td')) if b]  # Grabs all the B tags which contains the scores
@@ -77,12 +78,12 @@ class Data():
         #returns the details from the game
         elif mode == 'detail':
             row_arr= []
-            if self.year == 2018:
+            if self.year == 2018: #This is added due to different implementation in the html table in the link of 2017 and 2018
                 start = 7
             else:
                 start = 8
-            for i in range(start,len(data_frame)):
-                for index, row in data_frame[i].iterrows():
+            for i in range(start,len(data_frame)): #The range from this for loop is where most of the details are located at from the table
+                for index, row in data_frame[i].iterrows(): #It adds the details into an array
                     row_arr.append(row[1])
             return row_arr
         #should return a data frame object/Panda Object
@@ -90,12 +91,13 @@ class Data():
             return data_frame
         #should return the name of that match
         elif mode == 'name':
-            for i in range(6, len(match_name), 3):
-                if counter == match_number:
+            for i in range(6, len(match_name), 3):  # start: is where the titles start from the html. Iterator: every 3rd because that is where the title is located
+                if counter == match_number: #It would loop through every title till the counter matches the parameter match_number
                   return match_name[i].get_text()
                 counter = counter + 1
         #Score with details
         else:
+            #if user input is irrational
             raise Exception(r"modes are only 'score' \ 'name' \ 'data frame' \ 'detail'")
 
     def grabTeamScore(self,Team, match_number):
@@ -108,11 +110,11 @@ class Data():
         scoresarr = self.match(match_number)
         score_arr = []
         if Team == 'ub':  # if the parameter is B then is will return Buffalo scores
-            UBscores = re.findall(r'\d{1,2}(?=-)', str(scoresarr))
+            UBscores = re.findall(r'\d{1,2}(?=-)', str(scoresarr)) #matches strings that have a pattern of: [digits -]
             for score in UBscores:
                 score_arr.append(score)
         elif Team == 'opponent':  # if the parameter is M then it will return Michigan scores
-            OPPscores = re.findall(r'(?<=-)\d{1,2}', str(scoresarr))
+            OPPscores = re.findall(r'(?<=-)\d{1,2}', str(scoresarr)) #matches strings that have a pattern of: [- digits]
             for score in OPPscores:
                 score_arr.append(score)
         return score_arr
@@ -120,7 +122,7 @@ class Data():
 
     def convBStoString(self,bs_objectArr):
         '''
-        Converts Array of Beautiful soup objects into a Stirng
+        Converts Array of Beautiful soup objects into a String
         :param bs_objectArr: An array of beautiful soup objects
         :return: It will convert the parameter into a String object
         '''
